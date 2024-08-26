74679a1e536b6fd5bdcd7c4c2c00b433b1df727f
gvnberaldi <giovanniberaldi43@gmail.com>
2024-08-26 19:02:05 +0200

    SCRUM-28 Added application code that define the SQL schema and make the tests pass, added a docker-compose file and CI\CD pipeline updated to integrate database connection

--- tests/unit/test_export_sql_functionality_unit.py
+++ tests/unit/test_export_sql_functionality_unit.py
@@ -1,6 +1,7 @@
 import pytest
 import psycopg2
 
+from export_to_sql import *
 
 # Define the fixture for PostgreSQL connection and cursor
 @pytest.fixture(scope="module")
@@ -8,30 +9,8 @@ def postgres_connection():
     """
     Fixture to set up and tear down a PostgreSQL database connection for testing.
     """
-    # Retrieve connection details from environment variables, with defaults for local development
-    db_host = "localhost"
-    db_user = "postgres"
-    db_password = "admin"
-    db_name = "syslog"
-    db_port = 5432
-
-    # Establish a connection to the PostgreSQL server
-    conn = psycopg2.connect(
-        host=db_host,
-        user=db_user,
-        password=db_password,
-        dbname=db_name,
-        port=db_port
-    )
-    cursor = conn.cursor()
-
-    # Ensure a clean state before running tests
-    cursor.execute("DROP TABLE IF EXISTS syslog")
-
+    conn = get_db_connection()
     yield conn  # Provide the fixture value (connection) to the test function
-
-    # Clean up after tests
-    cursor.execute("DROP TABLE IF EXISTS syslog")
     conn.close()
 
 
@@ -40,15 +19,19 @@ def postgres_cursor(postgres_connection):
     """
     Fixture to provide a cursor for executing SQL queries.
     """
-    cursor = postgres_connection.cursor()
+    cursor = get_db_cursor(postgres_connection)
+    # Ensure a clean state before running tests
+    cursor.execute("DROP TABLE IF EXISTS syslog")
     yield cursor
+    # Clean up after tests
+    cursor.execute("DROP TABLE IF EXISTS syslog")
     cursor.close()
 
 
-def test_create_syslog_table(postgres_cursor):
+def test_create_syslog_table(postgres_connection, postgres_cursor):
 
     # Create the syslog table
-    create_syslog_table(postgres_cursor)
+    create_syslog_table(postgres_connection, postgres_cursor)
 
     # Verify that the table was created
     postgres_cursor.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'syslog')")
@@ -56,9 +39,9 @@ def test_create_syslog_table(postgres_cursor):
     assert result[0]  # Ensure the syslog table exists
 
 
-def test_syslog_schema_fields(postgres_cursor):
+def test_syslog_schema_fields(postgres_connection, postgres_cursor):
     # Create the syslog table
-    create_syslog_table(postgres_cursor)
+    create_syslog_table(postgres_connection, postgres_cursor)
 
     postgres_cursor.execute("""
             SELECT column_name, data_type, column_default, is_nullable
@@ -66,7 +49,6 @@ def test_syslog_schema_fields(postgres_cursor):
             WHERE table_name = 'syslog';
         """)
     columns = postgres_cursor.fetchall()
-    postgres_cursor.close()
 
     expected_columns = [
         ('id', 'integer', 'nextval(\'syslog_id_seq\'::regclass)', 'NO'),
